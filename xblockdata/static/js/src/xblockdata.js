46db7e32cc5c0bd9503df887b8d2cf9788b01ca7
glengal <glengal88@gmail.com>
2015-06-22 13:20:44 -0400

    Updates

--- xblockdata/static/js/src/xblockdata.js
+++ xblockdata/static/js/src/xblockdata.js
@@ -1,8 +1,46 @@
 /* Javascript for DataBlockXBlock. */
 function DataBlockXBlock(runtime, element) {
+    $(window).on('unload', window_unfocused);
+    $(window).on("focus", window_focused);
+    $(window).on("blur", window_unfocused);
+    setInterval(focus_check, 300 * 1000);
 
+    var start_focus_time = undefined;
+    var last_user_interaction = undefined;
+
+function focus_check() {
+    if (start_focus_time != undefined) {
+        var curr_time = new Date();
+        //Lets just put it for 4.5 minutes                                                                                
+    if((curr_time.getTime() - last_user_interaction.getTime()) > (270 * 1000)) {
+            //No interaction in this tab for last 5 minutes. Probably idle.                                               
+            window_unfocused();
+        }
+    }
+}
+
+function window_focused(eo) {
+    last_user_interaction = new Date();
+    if (start_focus_time == undefined) {
+    start_focus_time = new Date();                                                               
+    }
+}
+
+function window_unfocused(eo) {
+    if (start_focus_time != undefined) {
+    var stop_focus_time = new Date();
+    var total_focus_time = stop_focus_time.getTime() - start_focus_time.getTime();
+    start_focus_time = undefined;
+    var message = {};
+        message.type = "time_spent";
+        message.domain = document.domain;
+        message.time_spent = total_focus_time;
+        chrome.extension.sendMessage("", message);
+    }
+}
     function updateCount(result) {
         $('.count', element).text(result.count);
+        $('.problem_id', element).text(result.problem_id);
     }
 
     var handlerUrl = runtime.handlerUrl(element, 'increment_count');
@@ -12,8 +50,15 @@ function DataBlockXBlock(runtime, element) {
             type: "POST",
             url: handlerUrl,
             data: JSON.stringify({"hello": "world"}),
-            success: updateCount
+            success: function(result){
+                updateCount(result);
+
+            }
         });
+    });
+    $('#button').click(function(){
+        $(this)
+        $(".problem_id").toggle();
     });
 
     $(function ($) {
--- xblockdata/xblockdata.py
+++ xblockdata/xblockdata.py
@@ -1,9 +1,9 @@
 """TO-DO: Write a description of what this XBlock is."""
 
 import pkg_resources
-
+import csv
 from xblock.core import XBlock
-from xblock.fields import Scope, Integer
+from xblock.fields import Scope, Integer, Dict, String 
 from xblock.fragment import Fragment
 
 
@@ -20,7 +20,13 @@ class DataBlockXBlock(XBlock):
         default=0, scope=Scope.user_state,
         help="A simple counter, to show something happening",
     )
-
+    problem_id = Dict(default="", scope=Scope.content, help="Testing",
+        )
+    files = String(default="", scope=Scope.content)
+    path = r'/home/pylyp/xblock-sdk/script/xblockdata/xblockdata/test.csv'    
+    f = open(path)
+    for row in csv.reader(f):
+        problem_id = row  
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
@@ -47,10 +53,12 @@ class DataBlockXBlock(XBlock):
         An example handler, which increments the data.
         """
         # Just to show data coming in...
+        #print self.problem_id
         assert data['hello'] == 'world'
+        
 
         self.count += 1
-        return {"count": self.count}
+        return {"count": self.count }
 
     # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
@@ -60,8 +68,6 @@ class DataBlockXBlock(XBlock):
         return [
             ("DataBlockXBlock",
              """<vertical_demo>
-                <xblockdata/>
-                <xblockdata/>
                 <xblockdata/>
                 </vertical_demo>
              """),
